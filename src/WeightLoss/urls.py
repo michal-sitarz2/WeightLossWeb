a949d9040164c639cbde61901c2944cde8b7d523
Michal Sitarz <michalsitarz2001@gmail.com>
2021-03-03 14:10:09 +0000

    Pages App created, and add a simple home page view template.

--- src/WeightLoss/urls.py
+++ src/WeightLoss/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import homepage_view
 
 urlpatterns = [
+    path('', homepage_view, name="home"),
+    path('home/', homepage_view, name="home"),
     path('admin/', admin.site.urls),
 ]
