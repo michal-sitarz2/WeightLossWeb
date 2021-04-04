cbb59b1890340ee0c758de8ef7a905389f15eac3
Todor Ranchev <todomodo12@gmail.com>
2021-04-04 10:50:59 +0100

    Remove the inegrated Spoonacular

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import spoonacular as sp
+# import spoonacular as sp
 import requests
 
-def spoonacular_api_view(request):
-    api = sp.API("64dadb7cfd724991b7ea9987c4685790")
+# def spoonacular_api_view(request):
+#     api = sp.API("64dadb7cfd724991b7ea9987c4685790")
 
-    jokes_generator = api.get_a_random_food_joke()
-    data_jokes = jokes_generator.json()
+#     jokes_generator = api.get_a_random_food_joke()
+#     data_jokes = jokes_generator.json()
 
-    recipes_generator = api.get_random_recipes()
-    data_recipes = recipes_generator.json()
+#     recipes_generator = api.get_random_recipes()
+#     data_recipes = recipes_generator.json()
 
-    return render(request, "spoonacular_api.html", {
-        'joke': data_jokes['text'],
-        'recipes': data_recipes
-    })
+#     return render(request, "spoonacular_api.html", {
+#         'joke': data_jokes['text'],
+#         'recipes': data_recipes
+#     })
 
 def spoonacular_api_search_view(request):
     context = {}
