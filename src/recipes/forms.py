3f10fa53cbd3d5924f8c0e10a7082e303859a8e3
Todor Ranchev <todomodo12@gmail.com>
2021-04-06 15:24:43 +0100

    Make API looks better

--- src/recipes/forms.py
+++ src/recipes/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 
+class SearchRecipeForm(forms.Form):
+    # This is for a search input to look through the API 
+
+    search_query = forms.CharField(help_text="Search something like: Chicken with rice")
+
 class PreferencesForm(forms.Form):
     '''
     User-set information
