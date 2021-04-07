b620e32e2973326b10471eaf681a95b10e7c94f7
Michal Sitarz <michalsitarz2001@gmail.com>
2021-04-07 20:36:03 +0100

    Adjusted recipes table to fit the API information

--- src/recipes/admin.py
+++ src/recipes/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from .models import Recipe
 
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'protein', 'carbs', 'fats', 'vegetarian', 'vegan',
-                    'gluten_free', 'halaal', 'shellfish_free', 'lactose_free')
+    list_display = ('name',)
     search_fields = ('name',)
 
 admin.site.register(Recipe, RecipeAdmin)
--- src/recipes/models.py
+++ src/recipes/models.py
@@ -5,47 +5,63 @@ class Recipe(models.Model):
     # The meal name
     name = models.CharField(max_length=100, blank=False)
 
-    # What kind of meal this is. (Breakfast, Lunch, Dinner)
-    meal_type = models.CharField(max_length=100)
+    # ID given by spoonacular
+    recipe_id = models.IntegerField(validators=[MinValueValidator(0)], unique=True, blank=False)
 
+    # What kind of meal this is. (Breakfast, Lunch, Dinner)
+    meal_type = models.CharField(max_length=100, blank=False)
+
+    # Image URL
+    image_link = models.CharField(max_length=150, blank=True, null=True)
+
+    # Ingredients
+    ingredients = models.TextField(null=True)
+
+    # Servings
+    servings = models.IntegerField(validators=[MinValueValidator(0)], default=1)
 
     # Nutrition info
-    energy = models.FloatField(validators=[MinValueValidator(0)])
-    protein = models.FloatField(validators=[MinValueValidator(0)])
-    carbs = models.FloatField(validators=[MinValueValidator(0)])
-    fats = models.FloatField(validators=[MinValueValidator(0)])
+    #energy = models.FloatField(validators=[MinValueValidator(0)])
+    protein = models.FloatField(validators=[MinValueValidator(0)], blank=False)
+    carbs = models.FloatField(validators=[MinValueValidator(0)], blank=False)
+    fats = models.FloatField(validators=[MinValueValidator(0)], blank=False)
+    calories = models.FloatField(validators=[MinValueValidator(0)], blank=False)
+    saturated_fats = models.FloatField(validators=[MinValueValidator(0)], blank=False)
+    sugars = models.FloatField(validators=[MinValueValidator(0)], blank=False)
 
-    # Some simple tags to allow us to sort the meals for user preference.
-    # We might need more or we might want to reduce the number of tags.
 
-    vegetarian = models.BooleanField(default=False)
-    vegan = models.BooleanField(default=False)
-    gluten_free = models.BooleanField(default=False)
-    halaal = models.BooleanField(default=False)
-    shellfish_free = models.BooleanField(default=False)
-    lactose_free = models.BooleanField(default=False)
+    ### Some simple tags to allow us to sort the meals for user preference.
+    ### We might need more or we might want to reduce the number of tags.
+
+    # vegetarian = models.BooleanField(default=False)
+    # vegan = models.BooleanField(default=False)
+    # gluten_free = models.BooleanField(default=False)
+    # halaal = models.BooleanField(default=False)
+    # shellfish_free = models.BooleanField(default=False)
+    # lactose_free = models.BooleanField(default=False)
     
     def __str__(self):
         return self.name
 
-    # Lets us check if the meal meets the dietary requirements
-    def check_requirements(restrictions_list):
-        '''
-        Pass the requirements as a list of strings where each string
-        is a requirement that must be true. eg ['vegan', 'shellfish_free']
-        '''
-
-        requirements = {
-            'vegetarian':self.vegetarian,
-            'vegan':self.vegan,
-            'gluten_free':self.gluten_free,
-            'halaal':self.halaal,
-            'shellfish_free':self.shellfish_free,
-            'lactose_free':self.lactose_free
-            }
-
-        for restriction in restrictions_list:
-            # If any requirement is false, we return false as all must be true
-            if not requirements[restriction]:
-                return False
-        return True
+    #
+    # # Lets us check if the meal meets the dietary requirements
+    # def check_requirements(restrictions_list):
+    #     '''
+    #     Pass the requirements as a list of strings where each string
+    #     is a requirement that must be true. eg ['vegan', 'shellfish_free']
+    #     '''
+    #
+    #     requirements = {
+    #         'vegetarian':self.vegetarian,
+    #         'vegan':self.vegan,
+    #         'gluten_free':self.gluten_free,
+    #         'halaal':self.halaal,
+    #         'shellfish_free':self.shellfish_free,
+    #         'lactose_free':self.lactose_free
+    #         }
+    #
+    #     for restriction in restrictions_list:
+    #         # If any requirement is false, we return false as all must be true
+    #         if not requirements[restriction]:
+    #             return False
+    #     return True
